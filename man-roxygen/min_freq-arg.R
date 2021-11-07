cacb75ba57a0fbd341ed6041c2046c13e2e562b7
paul-sheridan <paul.sheridan.stats@gmail.com>
2021-11-07 18:10:07 -0500

    typo fix

--- man-roxygen/min_freq-arg.R
+++ man-roxygen/min_freq-arg.R
@@ -1,3 +1,2 @@
 #' @param min_freq Drop themes occurring less than this number of times from
-#' the analysis. The default `min_occurrence=1` results in no themes are
-#' discarded.
+#' the analysis. The default `min_freq=1` results in no themes are discarded.
